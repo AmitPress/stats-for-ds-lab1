26cd31da205ea0f170ae98e9503d064f78eb73de
AmitPress <amitkrsurjo@gmail.com>
2022-02-18 12:57:51 +0600

    solved 12.py

---
+++ solutions/12.py
@@ -0,0 +1,26 @@
+# problem statement: Given a string and an integer number n, remove characters from a string starting from zero up to n
+# and return a new string. N must be less than the length of the string. Read inputs from the user. Do
+# not use any built-in function.
+
+def solution(n,s):
+    sz = len(s)
+    if n>sz:
+        print("N is larger than the size of the string")
+        return
+    new_string = ""
+    for i, c in enumerate(s):
+        if not i<n:
+            new_string += c
+    return new_string
+if __name__ == '__main__':
+    n = int(input("Enter the number:\n"))
+    s = input("Enter the string:\n")
+    print(f"The new string is: {solution(n,s)}")
+
+# test:
+# Enter the number:
+# 5
+# Enter the string:
+# IAMTIRED
+# The new string is: RED
+        
